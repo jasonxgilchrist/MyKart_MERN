4479143ae0848d419154d5a95c3d642b35565d80
jasonxgilchrist <jasonxgilchrist@gmail.com>
2021-07-09 21:53:40 +0530

    Fetching data sucessfully from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+
+      setProduct(data)
+    }
+
+    fetchProduct()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
@@ -52,7 +63,7 @@ const ProductScreen = ({ match }) => {
               <Button
                 className='btn-block'
                 type='button'
-                disabled={product.countInStock == 0}>
+                disabled={product.countInStock === 0}>
                 Add to cart
               </Button>
             </ListGroup.Item>
