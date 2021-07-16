7d9b6e52a6c146e2045b909851b88a7d9842dfb1
jasonxgilchrist <jasonxgilchrist@gmail.com>
2021-07-16 14:13:50 +0530

    addeded config and models directories in backend

---
+++ backend/models/productModel.js
@@ -0,0 +1,74 @@
+import Mongoose from 'mongoose'
+
+const reviewSchema = Mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const productSchema = Mongoose.Schema(
+  {
+    user: {
+      type: Mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Srring,
+      required: true,
+    },
+    description: {
+      type: Srring,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const product = Mongoose.model('Product', productSchema)
+
+export default product
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const products = require('./data/products')
-const dotenv = require('dotenv')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -15,12 +19,14 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
+  const product = find((p) => p._id === req.params.id)
   res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 )
